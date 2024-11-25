28552c2ddb6aeb9254cd2a7830c09fa23382695b
Sinai-chan <chanjoana03@gmail.com>
2024-11-25 12:09:03 -0600

    primer commit

---
+++ lib/homepage.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flappy_bird_3/bird.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double birdYaxis = -1;
+
+  void jump() {
+    Timer.periodic(Duration(milliseconds: 500), (timer) {
+      setState(() {
+        birdYaxis += 0.1;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: GestureDetector(
+              onTap: jump,
+              child: AnimatedContainer(
+                alignment: Alignment(0, birdYaxis),
+                duration: Duration(milliseconds: 0),
+                color: Colors.blue,
+                child: MyBird(),
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            color: Colors.green,
+          ))
+        ],
+      ),
+    );
+  }
+}
